01fb8574beec466d36a9f0263d165dc4d49c8cef
paulajessica-dev <jessicafldasilva@gmail.com>
2025-09-13 10:36:38 -0300

    Fix schema

--- controllers/users.js
+++ controllers/users.js
@@ -28,10 +28,11 @@ const getSingle = async(req,res) => {
 const createUser = async(req,res) => {
     //#swagger.tags=['Users']
     const user = {            
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,    
         email: req.body.email,
-        username: req.body.username,    
-        name: req.body.name,
-        ipaddress: req.body.ipaddress
+        favoriteColor: req.body.favoriteColor,
+        birthday: req.body.birthday
     };
     const response = await mongodb.getDatabase().collection('users').insertOne(user);
     if (response.acknowledged) {
@@ -46,10 +47,11 @@ const updateUser = async(req,res) => {
     //#swagger.tags=['Users']
     const userId = new ObjectId(req.params.id);   
     const user = {
-        userName: req.body.userName,        
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,    
         email: req.body.email,
-        name: req.body.name,
-        ipaddress: req.body.ipaddress
+        favoriteColor: req.body.favoriteColor,
+        birthday: req.body.birthday
     };
     const response = await mongodb.getDatabase().collection('users').replaceOne({ _id: userId }, user);
     if (response.modifiedCount > 0) {
