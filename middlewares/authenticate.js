4a149bfca10975d85275f56ea5a1c63300ec0e3a
paulajessica-dev <jessicafldasilva@gmail.com>
2025-09-27 16:10:08 -0300

    change token

--- middlewares/authenticate.js
+++ middlewares/authenticate.js
@@ -1,12 +1,15 @@
-//middleware authenticateToken - verify if token is valided or expired and, create tokenblacklist
 const { isTokenBlacklisted } = require('../models/blacklist');
+const jwt = require('jsonwebtoken');
+const secret = process.env.SECRET_KEY;
 
 async function authenticateToken(req, res, next) {
     const token = req.headers['authorization']?.split(' ')[1];
     if (!token) return res.status(401).json({ message: 'Access denied. No token provided.' });    
+
     if (await isTokenBlacklisted(token)) {
         return res.status(401).json({ message: 'Token is invalid. Please login again.' });
     }
+
     try {
         const verified = jwt.verify(token, secret);
         req.user = verified;
@@ -14,6 +17,6 @@ async function authenticateToken(req, res, next) {
     } catch (err) {
         res.status(401).json({ message: 'Invalid or expired token' });
     }
-};
+}
 
 module.exports = authenticateToken;
