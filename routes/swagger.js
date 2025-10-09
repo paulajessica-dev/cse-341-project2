29cbfe4395c0a86fcf878aa67ce52c83a819583d
paulajessica-dev <jessicafldasilva@gmail.com>
2025-10-08 22:06:54 -0300

    Fix options

--- routes/swagger.js
+++ routes/swagger.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const swaggerUi = require('swagger-ui-express');
 const swaggerDocument = require('../swagger.json');
 
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument, {
+router.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument, {
     swaggerOptions: { withCredentials: true }
 }));
 
