3ca2473932c3214b4cd45dbaba90a77a8902e594
paulajessica-dev <jessicafldasilva@gmail.com>
2025-10-09 13:42:14 -0300

    alter if

--- middlewares/authenticate.js
+++ middlewares/authenticate.js
@@ -1,6 +1,6 @@
 const isAuthenticated = (req,res, next) => {
     const user = req.session.user;
-    if(!user || (!user.displayName && !user.username)){
+    if(user == null || user.displayName == null|| user.username == null){
         return res.status(401).json('You do not have access.')
     }
     next();
