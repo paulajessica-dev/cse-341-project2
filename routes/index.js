c687bca52187b7c9b2625e2bb74388cacdfcb2ed
paulajessica-dev <jessicafldasilva@gmail.com>
2025-10-08 21:37:48 -0300

    Add auth

--- routes/index.js
+++ routes/index.js
@@ -1,43 +1,19 @@
 const passport = require('passport');
 const router = require('express').Router();
 
-//login
-router.get('/login', passport.authenticate('github'));
 
-//callback github
-router.get('/github/callback', passport.authenticate('github', {
-  failureRedirect: '/api-docs'
-}), (req, res) => {
-  req.session.user = req.user;
-  res.redirect('/');
-});
-
-//logout
-router.get('/logout', (req, res, next) => {
-  req.logout(err => {
-    if (err) return next(err);
-    req.session.destroy(() => {
-      res.clearCookie('connect.sid');
-      res.redirect('/');
-    });
-  });
-});
-
-//swagger
-router.use('/api-docs', require('./swagger')); 
-
-//home
 router.get('/', (req, res) => {
-    const user = req.session.user;
-    if (user) {
-        res.send(`Logged in as ${user.displayName || user.username}`);
-    } else {
-        res.send('API Conectada! <a href="/login">Login com GitHub</a>');
-    }
+  
+  res.send(req.session?.user
+    ? `Logged in as ${req.session.user.displayName}`
+    : 'API Conectada - Logged Out');
 });
 
-//others routes
+
 router.use('/users', require('./users'));
 router.use('/songs', require('./songs'));
+router.use('/api-docs', require('./swagger'));
+
+module.exports = router;
 
 module.exports = router;
