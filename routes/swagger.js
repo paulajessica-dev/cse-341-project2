5df91f5b3967ee7b56a317902952838ba07ae155
paulajessica-dev <jessicafldasilva@gmail.com>
2025-10-09 12:25:17 -0300

    change swagger

--- routes/swagger.js
+++ routes/swagger.js
@@ -1,26 +1,7 @@
 const router = require('express').Router();
 const swaggerUi = require('swagger-ui-express');
-const path = require('path');
-
-router.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-  next();
-});
-
-router.use(
-  '/api-docs',
-  swaggerUi.serve,
-  swaggerUi.setup(null, {
-    swaggerOptions: {
-      url: 'https://cse-341-project2-7v19.onrender.com/swagger.json'
-    }
-  })
-);
-
-router.get('/swagger.json', (req, res) => {
-  res.sendFile(path.join(__dirname, '../swagger.json'));
-});
+const swaggerDocument = require('../swagger.json');
+router.use('/api-docs', swaggerUi.serve);
+router.get('/api-docs', swaggerUi.setup(swaggerDocument));
 
 module.exports = router;
--- swagger.js
+++ swagger.js
@@ -1,19 +1,15 @@
 const swaggerAutogen = require('swagger-autogen')();
 
 const doc = {
-  info: {
-    title: 'Users API',
-    description: 'Users API documentation',
-    version: '1.0.0'
-  },
-  host: 'cse-341-project2-7v19.onrender.com',
-  basePath: '/', 
-  schemes: ['https'],
-  consumes: ['application/json'], 
-  produces: ['application/json']
+    info: {
+        title: 'Users API',
+        description: 'Users API'
+    },
+    host: 'localhost:3001',
+    schemes: ['https']
 };
 
 const outputFile = './swagger.json';
-const endpointsFiles = ['./routes/index.js', './routes/users.js', './routes/songs.js'];
+const endpointsFiles = ['./routes/index.js'];
 
 swaggerAutogen(outputFile, endpointsFiles, doc);
