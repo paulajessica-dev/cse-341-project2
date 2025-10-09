f3cd66d00652234c309526b851185e3305ce87ed
paulajessica-dev <jessicafldasilva@gmail.com>
2025-10-09 09:50:38 -0300

    change routes

--- server.js
+++ server.js
@@ -44,7 +44,7 @@ app.use(cors({
   credentials: true,
   methods: ['GET','POST','PUT','DELETE','OPTIONS','PATCH']
 }));
-app.use('/', require('./routes/index'));
+app.use('/', require('./routes'));
 
 passport.use(new GitHubStrategy({
     clientID: process.env.GITHUB_CLIENT_ID,
