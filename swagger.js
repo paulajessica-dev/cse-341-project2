4df105064ed147147405354a7b22be56d85370fd
paulajessica-dev <jessicafldasilva@gmail.com>
2025-10-09 09:27:38 -0300

    change https

--- swagger.js
+++ swagger.js
@@ -3,7 +3,7 @@ const swaggerAutogen = require('swagger-autogen')();
 const doc = {
     info: { title: 'Users API', description: 'Users API' },
     host: 'localhost:3001',
-    schemes: ['http'],
+    schemes: ['https'],
 };
 
 const outputFile = './swagger.json';
