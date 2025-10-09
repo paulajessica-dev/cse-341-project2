2a7d8ef9e50cfd074d1c8879f94edcdfacdc6891
paulajessica-dev <jessicafldasilva@gmail.com>
2025-10-09 10:12:50 -0300

    change server

--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-
 const dotenv = require('dotenv');
 dotenv.config();
 const express = require('express');
@@ -10,94 +9,81 @@ const passport = require('passport');
 const session = require('express-session');
 const GitHubStrategy = require('passport-github2').Strategy;
 const swaggerRouter = require('./routes/swagger');
-app.use(cors());
-app.use(express.json());
 
-const port = 3001
-const secret = process.env.SECRET_KEY
+const port = 3001;
+const secret = process.env.SECRET_KEY;
 
+//middlewares
 app.use(express.json());
 app.use(bodyParser.json());
 
-app.use(passport.initialize());
+//cors
+app.use(cors({
+  origin: '*',
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'PATCH'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
 
-app.use(passport.session());
-
-app.use((req,res,next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader(
-        'Access-Control-Allow-Headers',
-        'Origin, X-Requested-With, Content-type, Accept, Z-Key, Authorization'
-    );
-    res.setHeader(
-        'Access-Control-Allow-Methods', 
-        'GET,POST,PUT,DELETE,OPTIONS, PATCH');
-    next();
-});
-
+//session before passport
 app.use(session({
   secret: secret || 'defaultsecret',
   resave: false,
   saveUninitialized: true,
 }));
 
-app.use(cors({
-  origin: '*', // https://cse-341-project2-7v19.onrender.com)
-  credentials: true,
-  methods: ['GET','POST','PUT','DELETE','OPTIONS','PATCH'],
-  allowedHeaders: ['Content-Type', 'Authorization']
-}));
-
-
+//passport
 app.use(passport.initialize());
 app.use(passport.session());
 
+//routes
 app.use('/', swaggerRouter);
 app.use('/', require('./routes'));
 
+//gitHub strategy
 passport.use(new GitHubStrategy({
-    clientID: process.env.GITHUB_CLIENT_ID,
-    clientSecret: process.env.GITHUB_CLIENT_SECRET,
-    callbackURL: process.env.CALLBACK_URL
+  clientID: process.env.GITHUB_CLIENT_ID,
+  clientSecret: process.env.GITHUB_CLIENT_SECRET,
+  callbackURL: process.env.CALLBACK_URL
 },
 function(accessToken, refreshToken, profile, done){
-    //User.findOrCreate({ gitHubId: profile.Id }, function (err, user) {
-    return done(null, profile)
-//})
-}
-));
+  return done(null, profile);
+}));
 
-passport.serializeUser((user, done) => {
-    done(null,user);
+passport.serializeUser((user, done) => done(null, user));
+passport.deserializeUser((user, done) => done(null, user));
+
+app.get('/', (req, res) => {
+  const user = req.session.user;
+  if (user) {
+    res.send(`Logged in as ${user.displayName || user.username}`);
+  } else {
+    res.send('Logged Out');
+  }
 });
-passport.deserializeUser((user, done) => {
-    done(null,user);
-});
 
-app.get('/', (req, res) => { res.send( req.session.user ===! undefined ? `Logged in as ${req.session.user.displayName || req.session.user.username}` : 'Logged Out')});
-
-app.get('/github/callback', passport.authenticate('github', {
-    failureRedirect: '/api-docs', session: false}),
-    (req, res) => {
-        req.session.user = req.user;
-        res.redirect('/');
-    }
+app.get('/github/callback',
+  passport.authenticate('github', { failureRedirect: '/api-docs', session: false }),
+  (req, res) => {
+    req.session.user = req.user;
+    res.redirect('/');
+  }
 );
 
 process.on('uncaughtException', (err, origin) => {
   console.error('Unhandled exception:', err);
   console.error('Origin:', origin);
-  process.exit(1); 
+  process.exit(1);
 });
 
+//data + server
 mongodb.iniDb((err) => {
-    if (err){
-        console.log(err);
-    } else {
-        console.log('Database connected');
-        app.listen(process.env.PORT || port, () => {
-            console.log('Server running on port ' + (process.env.PORT || port));
-        });
-    }
+  if (err) {
+    console.log(err);
+  } else {
+    console.log('Database connected');
+    app.listen(process.env.PORT || port, () => {
+      console.log('Server running on port ' + (process.env.PORT || port));
+    });
+  }
 });
-
