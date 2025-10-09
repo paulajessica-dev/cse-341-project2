f93a83d42ada69a87e1609c75c103fa502e16877
paulajessica-dev <jessicafldasilva@gmail.com>
2025-10-09 16:29:47 -0300

    alter server

--- server.js
+++ server.js
@@ -52,12 +52,6 @@ app.use(cors({
 app.use(passport.initialize());
 app.use(passport.session());
 
-app.use((req, res, next) => {
-      res.setHeader('Access-Control-Allow-Origin', '*');
-      res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
-      res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
-      next();
-  })
 
 //routes
 app.use('/', swaggerRouter);
