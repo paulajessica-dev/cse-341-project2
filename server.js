a34ad9562e89bd79b90efcc8c84eaa9559c0d1b2
paulajessica-dev <jessicafldasilva@gmail.com>
2025-10-09 10:03:07 -0300

    change server

--- server.js
+++ server.js
@@ -16,12 +16,8 @@ app.use(express.json());
 const port = 3001
 const secret = process.env.SECRET_KEY
 
+app.use(express.json());
 app.use(bodyParser.json());
-app.use(session({
-    secret: 'secret',
-    resave: false,
-    saveUninitialized: true,
-}));
 
 app.use(passport.initialize());
 
@@ -40,10 +36,21 @@ app.use((req,res,next) => {
 });
 
 app.use(cors({
-  origin: '*',
+  origin: '*', // https://cse-341-project2-7v19.onrender.com)
   credentials: true,
-  methods: ['GET','POST','PUT','DELETE','OPTIONS','PATCH']
+  methods: ['GET','POST','PUT','DELETE','OPTIONS','PATCH'],
+  allowedHeaders: ['Content-Type', 'Authorization']
 }));
+
+app.use(session({
+  secret: secret || 'defaultsecret',
+  resave: false,
+  saveUninitialized: true,
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.use('/', swaggerRouter);
 app.use('/', require('./routes'));
 
