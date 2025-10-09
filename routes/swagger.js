8052246d64a244bd7efe467567f935089073e690
paulajessica-dev <jessicafldasilva@gmail.com>
2025-10-09 14:35:10 -0300

    change swagger

--- routes/swagger.js
+++ routes/swagger.js
@@ -1,14 +1,21 @@
 const router = require('express').Router();
 const swaggerUi = require('swagger-ui-express');
-const swaggerDocument = require('../swagger.json');
+const path = require('path');
+
+
+router.get('/swagger.json', (req, res) => {
+  res.sendFile(path.join(__dirname, '../swagger.json'));
+});
+
+
 router.use(
   '/api-docs',
   swaggerUi.serve,
   swaggerUi.setup(null, {
     swaggerOptions: {
-      url: '/swagger.json',
-      withCredentials: true 
+      url: '/swagger.json' 
     }
   })
 );
+
 module.exports = router;
