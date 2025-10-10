b1e41936b458ed6b56978aa93a858aab8f17a19b
paulajessica-dev <jessicafldasilva@gmail.com>
2025-10-10 07:38:55 -0300

    change router

--- routes/songs.js
+++ routes/songs.js
@@ -8,7 +8,7 @@ const { isAuthenticated } = require('../middlewares/authenticate');
 router.get('/', songsController.getAll);
 router.get('/:id', songsController.getSong);
 router.post('/', isAuthenticated,songValidations, validateRequest, songsController.createSong);
-router.put('/:id', isAuthenticated, validateRequest.updateSong);
+router.put('/:id', isAuthenticated,validateRequest,songsController.updateSong);
 router.delete('/:id', isAuthenticated,validateRequest,songsController.deleteSong);
 
 
