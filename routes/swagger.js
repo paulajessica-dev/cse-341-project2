6ba8d7e1d3ca43ba19bb299d6137642f60cd622a
paulajessica-dev <jessicafldasilva@gmail.com>
2025-10-09 21:35:57 -0300

    change swagger.js

--- routes/swagger.js
+++ routes/swagger.js
@@ -1,6 +1,8 @@
+const router = require('express').Router();
 const swaggerUi = require('swagger-ui-express');
 const swaggerDocument = require('../swagger.json');
 
+// Middleware de autenticação
 function ensureAuthenticated(req, res, next) {
   if (req.isAuthenticated() || req.session.user) {
     return next();
@@ -8,10 +10,12 @@ function ensureAuthenticated(req, res, next) {
   return res.status(403).send('You do not have access.');
 }
 
+// Serve swagger.json
 router.get('/swagger.json', (req, res) => {
   res.json(swaggerDocument);
 });
 
+// Swagger UI protegido
 router.use(
   '/api-docs',
   ensureAuthenticated,
@@ -19,11 +23,13 @@ router.use(
   swaggerUi.setup(swaggerDocument, {
     swaggerOptions: {
       url: '/swagger.json',
-      withCredentials: true, 
+      withCredentials: true,
       requestInterceptor: (req) => {
-        req.credentials = 'include'; 
+        req.credentials = 'include'; // envia cookie
         return req;
-      }
-    }
+      },
+    },
   })
 );
+
+module.exports = router;
