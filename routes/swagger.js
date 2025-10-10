0e829430c26dfe2dba6c9610f1de554d22be2437
paulajessica-dev <jessicafldasilva@gmail.com>
2025-10-10 08:10:28 -0300

    change swagger

--- routes/swagger.js
+++ routes/swagger.js
@@ -3,12 +3,12 @@ const router = express.Router();
 const swaggerUi = require('swagger-ui-express');
 const swaggerDocument = require('../swagger.json');
 
-// rota para servir o JSON do Swagger
+
 router.get('/swagger.json', (req, res) => {
   res.json(swaggerDocument);
 });
 
-// rota para exibir a UI do Swagger
+
 router.use(
   '/api-docs',
   swaggerUi.serve,
