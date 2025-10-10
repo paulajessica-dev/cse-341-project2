704d4a55c3fb8e9f8965f7c0f3da4d02029bcae0
paulajessica-dev <jessicafldasilva@gmail.com>
2025-10-10 07:22:27 -0300

    alter swagger

--- routes/swagger.js
+++ routes/swagger.js
@@ -3,32 +3,19 @@ const router = express.Router();
 const swaggerUi = require('swagger-ui-express');
 const swaggerDocument = require('../swagger.json');
 
-
-// function ensureAuthenticated(req, res, next) {
-//   if (req.isAuthenticated() || req.session.user) {
-//     return next();
-//   }
-//   return res.status(403).send('You do not have access.');
-// }
-
-
+// rota para servir o JSON do Swagger
 router.get('/swagger.json', (req, res) => {
   res.json(swaggerDocument);
 });
 
-
+// rota para exibir a UI do Swagger
 router.use(
   '/api-docs',
-  ensureAuthenticated, 
   swaggerUi.serve,
   swaggerUi.setup(swaggerDocument, {
     swaggerOptions: {
-      url: '/swagger.json', 
-      withCredentials: true, 
-      requestInterceptor: (req) => {
-        req.withCredentials = true; 
-        return req;
-      },
+      url: '/swagger.json',
+      withCredentials: true,
     },
   })
 );
